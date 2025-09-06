1cf013fc7c7a444ce172aa216a5adec00d8fb899
Krishna U <krishnaaudumula@gmail.com>
2025-09-06 16:50:39 +0530

    3495. Minimum Operations to Make Array Elements Zero

--- 3400-3499/3495. Minimum Operations to Make Array Elements Zero/Solution.java
+++ 3400-3499/3495. Minimum Operations to Make Array Elements Zero/Solution.java
@@ -1,16 +1,5 @@
 class Solution {
-  public long minOperations(int[][] queries) {
-    long ans = 0;
-    for (int[] query : queries) {
-      final int l = query[0];
-      final int r = query[1];
-      ans += (getOperations(r) - getOperations(l - 1) + 1) / 2;
-    }
-    return ans;
-  }
-
-  // Returns the number of operations required for [1, n].
-  private long getOperations(int n) {
+    private long getOperations(int n) {
     long res = 0;
     int ops = 0;
     for (int powerOfFour = 1; powerOfFour <= n; powerOfFour *= 4) {
@@ -20,4 +9,15 @@ class Solution {
     }
     return res;
   }
+    public long minOperations(int[][] queries) {
+        long ans = 0;
+    for (int[] query : queries) {
+      final int l = query[0];
+      final int r = query[1];
+      ans += (getOperations(r) - getOperations(l - 1) + 1) / 2;
+    }
+    return ans;
+    }
+    
+    
 }
